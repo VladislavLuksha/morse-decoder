4bd3a45c878f85ff45f1b6b74d66d84535c9e0fa
Vladislav <vladislavluksha99@gmail.com>
2020-10-25 18:26:18 +0300

    feat: implementation of function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultString = '';
+    let actualString = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        if (expr[i] === '*') { 
+            resultString += ' '; 
+            continue;
+        }
+        actualString = '';
+        for (let j = 0; j < 10; j += 2) {
+            if (expr[j + i] === '0') continue;
+            else if (expr[j + i] === '1' && expr[j + i + 1] === '0') actualString += '.';
+            else if (expr[j + i] === '1' && expr[j + i + 1] === '1') actualString += '-';
+        }
+        resultString += MORSE_TABLE[actualString];
+    }
+    return resultString;
 }
 
 module.exports = {
